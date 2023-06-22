3a9c93c7b37908159c40be1dfedaa2d3b940fcb7
Jiaqing Xie <jiaxie@student.ethz.ch>
2023-06-22 14:50:03 +0200

    have bugs

---
+++ DeepPurpose2.0/dataset.py
@@ -0,0 +1,136 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from typing import Union, List, Tuple, Dict, Any, Sequence
+from pathlib import Path
+import lmdb
+import pickle as pkl
+from utils import TAPETokenizer
+from utils.registry import registry
+
+def dataset_factory(data_file: Union[str, Path], *args, **kwargs) -> Dataset:
+    data_file = Path(data_file)
+    if not data_file.exists():
+        raise FileNotFoundError(data_file)
+    if data_file.suffix == '.lmdb':
+        return LMDBDataset(data_file, *args, **kwargs)
+    # elif data_file.suffix in {'.fasta', '.fna', '.ffn', '.faa', '.frn'}:
+    #     return FastaDataset(data_file, *args, **kwargs)
+    # elif data_file.suffix == '.json':
+    #     return JSONDataset(data_file, *args, **kwargs)
+    # elif data_file.is_dir():
+    #     return NPZDataset(data_file, *args, **kwargs)
+    else:
+        raise ValueError(f"Unrecognized datafile type {data_file.suffix}")
+
+def pad_sequences(sequences: Sequence, constant_value=0, dtype=None) -> np.ndarray:
+    batch_size = len(sequences)
+    shape = [batch_size] + np.max([seq.shape for seq in sequences], 0).tolist()
+
+    if dtype is None:
+        dtype = sequences[0].dtype
+
+    if isinstance(sequences[0], np.ndarray):
+        array = np.full(shape, constant_value, dtype=dtype)
+    elif isinstance(sequences[0], torch.Tensor):
+        array = torch.full(shape, constant_value, dtype=dtype)
+
+    for arr, seq in zip(array, sequences):
+        arrslice = tuple(slice(dim) for dim in seq.shape)
+        arr[arrslice] = seq
+
+    return array
+
+class LMDBDataset(Dataset): # This part is the same as the part in TorchDrug
+    """Creates a dataset from an lmdb file.
+    Args:
+        data_file (Union[str, Path]): Path to lmdb file.
+        in_memory (bool, optional): Whether to load the full dataset into memory.
+            Default: False.
+        
+    """
+
+    def __init__(self,
+                 data_file: Union[str, Path],
+                 in_memory: bool = False):
+
+        data_file = Path(data_file)
+        if not data_file.exists():
+            raise FileNotFoundError(data_file)
+
+        env = lmdb.open(str(data_file), max_readers=1, readonly=True,
+                        lock=False, readahead=False, meminit=False)
+
+        with env.begin(write=False) as txn:
+            num_examples = pkl.loads(txn.get(b'num_examples'))
+
+        if in_memory:
+            cache = [None] * num_examples
+            self._cache = cache
+
+        self._env = env
+        self._in_memory = in_memory
+        self._num_examples = num_examples
+
+    def __len__(self) -> int:
+        return self._num_examples
+
+    def __getitem__(self, index: int):
+        if not 0 <= index < self._num_examples:
+            raise IndexError(index)
+
+        if self._in_memory and self._cache[index] is not None:
+            item = self._cache[index]
+        else:
+            with self._env.begin(write=False) as txn:
+                item = pkl.loads(txn.get(str(index).encode()))
+                if 'id' not in item:
+                    item['id'] = str(index)
+                if self._in_memory:
+                    self._cache[index] = item
+        return item
+
+@registry.register_task('fluorescence')
+class FluorescenceDataset(Dataset):
+
+    def __init__(self,
+                 data_path: Union[str, Path],
+                 split: str,
+                 tokenizer: Union[str, TAPETokenizer] = 'iupac',
+                 in_memory: bool = False):
+
+        if split not in ('train', 'valid', 'test'):
+            raise ValueError(f"Unrecognized split: {split}. "
+                             f"Must be one of ['train', 'valid', 'test']")
+        if isinstance(tokenizer, str):
+            tokenizer = TAPETokenizer(vocab=tokenizer)
+        self.tokenizer = tokenizer
+
+        data_path = Path(data_path)
+        data_file = f'fluorescence/fluorescence_{split}.lmdb'
+        self.data = dataset_factory(data_path / data_file, in_memory)
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        item = self.data[index]
+        token_ids = self.tokenizer.encode(item['primary'])
+        input_mask = np.ones_like(token_ids)
+        return token_ids, input_mask, float(item['log_fluorescence'][0])
+
+    def collate_fn(self, batch: List[Tuple[Any, ...]]) -> Dict[str, torch.Tensor]:
+        input_ids, input_mask, fluorescence_true_value = tuple(zip(*batch))
+        input_ids = torch.from_numpy(pad_sequences(input_ids, 0))
+        input_mask = torch.from_numpy(pad_sequences(input_mask, 0))
+        fluorescence_true_value = torch.FloatTensor(fluorescence_true_value)  # type: ignore
+        fluorescence_true_value = fluorescence_true_value.unsqueeze(1)
+
+        return {'input_ids': input_ids,
+                'input_mask': input_mask,
+                'targets': fluorescence_true_value}
+    
+# if __name__== "__main__":
+#     dataset = FluorescenceDataset("/home/jiaqing/Desktop/DeepPurposePlusPlus/DeepPurpose2.0/data", "train") 
+#     batch = dataset.collate_fn([dataset[1]])
+#     print(batch['targets'])
--- DeepPurpose2.0/models/gcn.py
+++
@@ -1,1 +0,0 @@
-## Need to be done, with GCN and Hyper-param Tuning
