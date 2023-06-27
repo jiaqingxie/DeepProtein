53abbf4ef1f409c0e7b3ac2f179a56c3203e1c52
Jiaqing Xie <jiaxie@student.ethz.ch>
2023-06-27 15:27:52 +0200

    finish CNN + Fluro

--- test.py
+++ test.py
@@ -8,21 +8,33 @@ import DeepPurpose_PP.ProteinPred as models
 if __name__ == "__main__":
 
 
-
     target_encoding = "CNN"
 
     path = os.getcwd()
-    # 1. Test on FluorescenceDataset
-    train_fluo = FluorescenceDataset(path + '/DeepPurpose_PP/data', 'valid')
+    #  Test on FluorescenceDataset
+    train_fluo = FluorescenceDataset(path + '/DeepPurpose_PP/data', 'train')
     train_protein_processed, train_target, train_protein_idx  = collate_fn(train_fluo)
 
+    valid_fluo = FluorescenceDataset(path + '/DeepPurpose_PP/data', 'valid')
+    valid_protein_processed, valid_target, valid_protein_idx  = collate_fn(valid_fluo)
 
-    train, val, test = utils.data_process(X_target = train_protein_processed, y = train_target, target_encoding = target_encoding, 
+    test_fluo = FluorescenceDataset(path + '/DeepPurpose_PP/data', 'test')
+    test_protein_processed, test_target, test_protein_idx  = collate_fn(test_fluo)
+
+    train, _, _ = utils.data_process(X_target = train_protein_processed, y = train_target, target_encoding = target_encoding, 
                                         # drug_encoding= drug_encoding,
-                                split_method='random',frac=[0.7,0.1,0.2],
+                                split_method='random',frac=[0.9998,1e-4,1e-4],
                                 random_seed = 1)
-
-    print(train[:1])
+    
+    _, val, _ = utils.data_process(X_target = valid_protein_processed, y = valid_target, target_encoding = target_encoding, 
+                                        # drug_encoding= drug_encoding,
+                                split_method='random',frac=[1e-4,0.9998,1e-4],
+                                random_seed = 1)
+    
+    _, _, test = utils.data_process(X_target = test_protein_processed, y = test_target, target_encoding = target_encoding, 
+                                        # drug_encoding= drug_encoding,
+                                split_method='random',frac=[1e-4,1e-4,0.9998],
+                                random_seed = 1)
 
 
     config = generate_config(target_encoding = target_encoding, 
@@ -31,7 +43,6 @@ if __name__ == "__main__":
                          LR = 0.0008, 
                          batch_size = 128,
                         )
-    
-    
+
     model = models.model_initialize(**config)
     model.train(train, val, test)
