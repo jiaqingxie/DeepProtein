acd97f23bc42df7df4b10cbf9e12663126521218
JiaX-TCS <jiaxie@student.ethz.ch>
2024-09-18 21:53:51 +0200

    add toy_data for PPI testing

--- train/PPI_test.py
+++ train/PPI_test.py
@@ -12,3 +12,24 @@ from ProB.dataset import *
 import ProB.utils as utils
 import ProB.PPI as models
 
+from DeepPurpose import PPI as models
+from DeepPurpose.utils import *
+from DeepPurpose.dataset import *
+
+X_targets, X_targets_, y = read_file_training_dataset_protein_protein_pairs("toy_data/ppi.txt")
+
+target_encoding = 'CNN'
+train, val, test = data_process(X_target = X_targets, X_target_ = X_targets_, y = y,
+			    target_encoding = target_encoding,
+			    split_method='random',
+			    random_seed = 1)
+
+config = generate_config(target_encoding = target_encoding,
+                         cls_hidden_dims = [512],
+                         train_epoch = 20,
+                         LR = 0.001,
+                         batch_size = 128,
+                        )
+
+model = models.model_initialize(**config)
+model.train(train, val, test)
