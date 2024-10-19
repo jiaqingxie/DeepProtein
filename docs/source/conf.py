e9b4b4c5d9f53788b8a9cefcddd1554be838f2c4
JiaX-TCS <jiaxie@student.ethz.ch>
2024-10-19 11:40:10 +0200

    new

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ html_theme_options = {
 }
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = '../build/html/_images/deeppurpose_pp_logo.png'
+html_logo = '../build/html/_images/deepprotein_logo.png'
 # html_context = {
 #     'css_files': [
 #   	   'https://fonts.googleapis.com/css?family=Raleway',
@@ -50,8 +50,8 @@ html_logo = '../build/html/_images/deeppurpose_pp_logo.png'
 
 
 
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 
 # The master toctree document.
-master_doc = 'index'
+# master_doc = 'index'
