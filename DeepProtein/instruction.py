8931d3569420db06e91c126e9f6adf4aafe8d763
JiaX-TCS <jiaxie@student.ethz.ch>
2025-02-20 12:49:39 +0100

    add solubility, subcellular and binary

--- DeepProtein/instruction.py
+++ DeepProtein/instruction.py
@@ -7,7 +7,13 @@ def get_example(dataset_name):
 
         "stability": ["You should return a floating-point number.", "protein stability"],
 
-        "solubility": ["You should return an integer (0 or 1) where 0 is not soluble and 1 is soluble", "protein solubility"]
+        "solubility": ["You should return an integer (0 or 1) where 0 is not soluble and 1 is soluble", "protein solubility"],
+
+        "subcellular": ["You should choose an integer within (0, 1, 2, 3, 4, 5, 6, 7, 8, 9) to indicate the protein's location.", "location"],
+
+        "subcellular_binary": ["You should return an integer (0 or 1) where 0 is membrane-bound and 1 is soluble.", "location"],
+
+        "tap": ["You should return a floating-point number.", "developability"],
 
     }
     return instruction_list[dataset_name]
