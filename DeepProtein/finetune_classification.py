c0ed16e02e12011872d75d2c0185abf85a21c458
JiaX-TCS <jiaxie@student.ethz.ch>
2025-03-08 11:25:46 +0100

    finetune tasks

--- DeepProtein/finetune_classification.py
+++ DeepProtein/finetune_classification.py
@@ -1,16 +1,10 @@
 import os
 import sys
 import numpy as np
-
+import re
 import torch
 import torch.nn as nn
-import re
-
-module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-from DeepProtein.dataset import *
+from sklearn.metrics import accuracy_score
 
 from transformers import (
     T5Config,
@@ -21,27 +15,32 @@ from transformers import (
     Trainer,
     DataCollatorWithPadding,
 )
+
+
+
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from DeepProtein.dataset import *
 from datasets import Dataset
 
-
-
-train = Beta_lactamase(os.getcwd() + '/DeepProtein/data', split='train')
-valid = Beta_lactamase(os.getcwd() + '/DeepProtein/data', split='valid')
+train = Solubility(os.getcwd() + '/DeepProtein/data', split='train')
+valid = Solubility(os.getcwd() + '/DeepProtein/data', split='valid')
 
 
 
-class T5RegressionModel(PreTrainedModel):
-
+class T5ClassificationModel(PreTrainedModel):
     config_class = T5Config
 
-    def __init__(self, config, d_model=None):
+    def __init__(self, config, d_model=None, num_classes=10):
         super().__init__(config)
+        self.num_classes = num_classes
 
         self.encoder = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
 
-
         hidden_dim = d_model if d_model is not None else config.d_model
-        self.regression_head = nn.Linear(hidden_dim, 1)
+        self.classification_head = nn.Linear(hidden_dim, num_classes)
 
     def forward(
         self,
@@ -55,45 +54,49 @@ class T5RegressionModel(PreTrainedModel):
 
         mask = attention_mask.unsqueeze(-1)
         pooled_output = (hidden_states * mask).sum(dim=1) / mask.sum(dim=1)
-        logits = self.regression_head(pooled_output).squeeze(-1)  # [batch_size]
-
+        logits = self.classification_head(pooled_output)  # [batch_size, num_classes]
 
         loss = None
         if labels is not None:
-            labels = labels.float()
-            loss = nn.MSELoss()(logits, labels)
+            labels = labels.long()
+            loss = nn.CrossEntropyLoss()(logits, labels)
 
         return {
             "loss": loss,
             "logits": logits
         }
 
+
+
 def build_fluo_data():
     data_list = []
     for i in range(len(train)):
-        seq, lab = train[i]   # (str, float)
+        seq, lab = train[i]
+        lab = int(lab)
         data_list.append({
             "sequence": seq,
-            "label": float(lab),   # 转成 float
+            "label": lab,
         })
     return data_list
 
 def build_fluo_valid():
     data_list = []
     for i in range(len(valid)):
-        seq, lab = valid[i]   # (str, float)
+        seq, lab = valid[i]
+        lab = int(lab)
         data_list.append({
             "sequence": seq,
-            "label": float(lab),   # 转成 float
+            "label": lab,
         })
     return data_list
+
 
 def main():
     fluo_data_list = build_fluo_data()
     fluo_valid_data_list = build_fluo_valid()
 
     train_dataset = Dataset.from_list(fluo_data_list)
-    val_dataset   = Dataset.from_list(fluo_valid_data_list)
+    val_dataset = Dataset.from_list(fluo_valid_data_list)
 
     MODEL_NAME = "Rostlab/prot_t5_xl_uniref50"
     tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True, do_lower_case=False)
@@ -102,41 +105,41 @@ def main():
         seq_list = [" ".join(list(re.sub(r"[UZOB]", "X", sequence))) for sequence in examples["sequence"]]
         out = tokenizer(seq_list, padding=True, truncation=True, max_length=300, return_tensors="pt")
         out["labels"] = examples["label"]
-
         return out
 
     train_dataset = train_dataset.map(tokenize_fn, batched=True)
-    val_dataset   = val_dataset.map(tokenize_fn, batched=True)
-
+    val_dataset = val_dataset.map(tokenize_fn, batched=True)
 
     train_dataset.set_format("torch")
     val_dataset.set_format("torch")
 
-    # 7) DataCollator
     data_collator = DataCollatorWithPadding(tokenizer)
 
 
     config = T5Config.from_pretrained(MODEL_NAME)
-    model = T5RegressionModel.from_pretrained(
+    num_classes = 2
+
+    model = T5ClassificationModel.from_pretrained(
         MODEL_NAME,
         config=config,
+        num_classes=num_classes,
         torch_dtype=torch.bfloat16
     )
 
+
     def compute_metrics(eval_pred):
-        predictions, labels = eval_pred
-        predictions = predictions.flatten()
-        labels      = labels.flatten()
-        mse = ((predictions - labels)**2).mean().item()
-        return {"mse": mse}
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        acc = accuracy_score(labels, predictions)
+        return {"accuracy": acc}
 
     training_args = TrainingArguments(
         output_dir="/cluster/scratch/jiaxie/checkpoints",
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        num_train_epochs=50,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        evaluation_strategy="no",
+        save_strategy="no",
+        num_train_epochs=30,
+        per_device_train_batch_size=10,
+        per_device_eval_batch_size=10,
         learning_rate=1e-4,
         weight_decay=0.0,
         logging_steps=10,
@@ -146,6 +149,7 @@ def main():
         fp16=False,
         seed=42,
     )
+
 
     trainer = Trainer(
         model=model,
@@ -159,19 +163,21 @@ def main():
 
     trainer.train()
 
-    save_path = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Beta"
+
+    save_path = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Solubility"
     model.save_pretrained(save_path)
     tokenizer.save_pretrained(save_path)
 
     from huggingface_hub import upload_folder
 
-    local_folder = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Beta"
-    repo_id = "jiaxie/DeepProtT5-Beta"
+    local_folder = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Solubility"
+    repo_id = "jiaxie/DeepProtT5-SubCellular-Solubility"
 
     upload_folder(
         folder_path=local_folder,
         repo_id=repo_id,
         repo_type="model"
     )
+
 if __name__ == "__main__":
     main()
--- DeepProtein/finetune_pair_regression.py
+++ DeepProtein/finetune_pair_regression.py
@@ -1,16 +1,8 @@
+print("yes")
 import os
 import sys
-import numpy as np
 
-import torch
 import torch.nn as nn
-import re
-
-module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-from DeepProtein.dataset import *
 
 from transformers import (
     T5Config,
@@ -21,35 +13,44 @@ from transformers import (
     Trainer,
     DataCollatorWithPadding,
 )
+print("yes")
+
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from DeepProtein.dataset import *
 from datasets import Dataset
+print("yes")
+
+from tdc.utils import retrieve_label_name_list
+from tdc.single_pred import Develop
+
+data = Develop(name='SAbDab_Chen')
+split = data.get_split()
+
+train_antibody_1, train_antibody_2 = to_two_seq(split, 'train', 'Antibody', sep=",")
+valid_antibody_1, valid_antibody_2 = to_two_seq(split, 'valid', 'Antibody', sep=",")
+
+y_train, y_valid, y_test = split['train']['Y'], split['valid']['Y'], split['test']['Y']
+
+train= list(zip(train_antibody_1, train_antibody_2, y_train))
+valid = list(zip(valid_antibody_1, valid_antibody_2, y_valid))
 
 
-
-train = Beta_lactamase(os.getcwd() + '/DeepProtein/data', split='train')
-valid = Beta_lactamase(os.getcwd() + '/DeepProtein/data', split='valid')
+print("yes")
 
 
-
-class T5RegressionModel(PreTrainedModel):
-
+class T5PairRegressionModel(PreTrainedModel):
     config_class = T5Config
 
     def __init__(self, config, d_model=None):
         super().__init__(config)
-
         self.encoder = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
-
-
         hidden_dim = d_model if d_model is not None else config.d_model
         self.regression_head = nn.Linear(hidden_dim, 1)
 
-    def forward(
-        self,
-        input_ids=None,
-        attention_mask=None,
-        labels=None,
-        **kwargs
-    ):
+    def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
         encoder_outputs = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
         hidden_states = encoder_outputs.last_hidden_state
 
@@ -57,87 +58,95 @@ class T5RegressionModel(PreTrainedModel):
         pooled_output = (hidden_states * mask).sum(dim=1) / mask.sum(dim=1)
         logits = self.regression_head(pooled_output).squeeze(-1)  # [batch_size]
 
-
         loss = None
         if labels is not None:
-            labels = labels.float()
+            labels = labels.to(torch.bfloat16)
             loss = nn.MSELoss()(logits, labels)
 
-        return {
-            "loss": loss,
-            "logits": logits
-        }
+        return {"loss": loss, "logits": logits}
+
 
 def build_fluo_data():
     data_list = []
     for i in range(len(train)):
-        seq, lab = train[i]   # (str, float)
+        seq1, seq2, lab = train[i]
         data_list.append({
-            "sequence": seq,
-            "label": float(lab),   # 转成 float
+            "sequence1": seq1,
+            "sequence2": seq2,
+            "label": float(lab),
         })
     return data_list
 
 def build_fluo_valid():
     data_list = []
     for i in range(len(valid)):
-        seq, lab = valid[i]   # (str, float)
+        seq1, seq2, lab = valid[i]
         data_list.append({
-            "sequence": seq,
-            "label": float(lab),   # 转成 float
+            "sequence1": seq1,
+            "sequence2": seq2,
+            "label": float(lab),
         })
     return data_list
 
+
 def main():
+
     fluo_data_list = build_fluo_data()
     fluo_valid_data_list = build_fluo_valid()
 
     train_dataset = Dataset.from_list(fluo_data_list)
-    val_dataset   = Dataset.from_list(fluo_valid_data_list)
+    val_dataset = Dataset.from_list(fluo_valid_data_list)
 
     MODEL_NAME = "Rostlab/prot_t5_xl_uniref50"
     tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True, do_lower_case=False)
 
     def tokenize_fn(examples):
-        seq_list = [" ".join(list(re.sub(r"[UZOB]", "X", sequence))) for sequence in examples["sequence"]]
-        out = tokenizer(seq_list, padding=True, truncation=True, max_length=300, return_tensors="pt")
+        seq1_list = [" ".join(list(re.sub(r"[UZOB]", "X", s1[:200]))) for s1 in examples["sequence1"]]
+        seq2_list = [" ".join(list(re.sub(r"[UZOB]", "X", s2[:200]))) for s2 in examples["sequence2"]]
+        out = tokenizer(
+            seq1_list,
+            seq2_list,
+            padding=True,
+            truncation=True,
+            max_length=800,
+            return_tensors="pt"
+        )
         out["labels"] = examples["label"]
-
         return out
 
     train_dataset = train_dataset.map(tokenize_fn, batched=True)
-    val_dataset   = val_dataset.map(tokenize_fn, batched=True)
-
+    val_dataset = val_dataset.map(tokenize_fn, batched=True)
 
     train_dataset.set_format("torch")
     val_dataset.set_format("torch")
 
-    # 7) DataCollator
+
     data_collator = DataCollatorWithPadding(tokenizer)
 
 
     config = T5Config.from_pretrained(MODEL_NAME)
-    model = T5RegressionModel.from_pretrained(
+    model = T5PairRegressionModel.from_pretrained(
         MODEL_NAME,
         config=config,
         torch_dtype=torch.bfloat16
     )
 
+
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
         predictions = predictions.flatten()
-        labels      = labels.flatten()
-        mse = ((predictions - labels)**2).mean().item()
+        labels = labels.flatten()
+        mse = ((predictions - labels) ** 2).mean().item()
         return {"mse": mse}
 
     training_args = TrainingArguments(
         output_dir="/cluster/scratch/jiaxie/checkpoints",
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        num_train_epochs=50,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        learning_rate=1e-4,
+        evaluation_strategy="no",
+        save_strategy="no",
+        num_train_epochs=200,
+        per_device_train_batch_size=10,
+        per_device_eval_batch_size=10,
+        learning_rate=5e-4,
         weight_decay=0.0,
         logging_steps=10,
         gradient_accumulation_steps=1,
@@ -145,7 +154,9 @@ def main():
         bf16=True,
         fp16=False,
         seed=42,
+        lr_scheduler_type="constant",
     )
+
 
     trainer = Trainer(
         model=model,
@@ -159,19 +170,17 @@ def main():
 
     trainer.train()
 
-    save_path = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Beta"
+
+    save_path = "/cluster/scratch/jiaxie/.cache/DeepProtT5-SAbDab-Chen"
     model.save_pretrained(save_path)
     tokenizer.save_pretrained(save_path)
 
     from huggingface_hub import upload_folder
-
-    local_folder = "/cluster/scratch/jiaxie/.cache/DeepProtT5-Beta"
-    repo_id = "jiaxie/DeepProtT5-Beta"
-
     upload_folder(
-        folder_path=local_folder,
-        repo_id=repo_id,
+        folder_path=save_path,
+        repo_id="jiaxie/DeepProtT5-SAbDab-Chen",
         repo_type="model"
     )
+
 if __name__ == "__main__":
     main()
